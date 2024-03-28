019a286333371ae37e7382c37c737abeaecb19c7
Sara S Camilleri <ssancam391@g.educaand.es>
2024-03-28 12:25:56 +0100

    terminar menú de usuario

--- src/Main.kt
+++ src/Main.kt
@@ -39,4 +39,47 @@ fun main() {
     println("Libros prestados: $librosPrestados")
     println("Todos los libros: $todosLibros")
 
+    /**
+     * Mostrar menú
+     * @return Devuleve el número correspondiente a la operación elegida por el usuario.
+     */
+    fun mostrarMenu(): Int {
+        println(
+            "Bienvenido al gestor de la biblioteca.\nIndica que operación quieres realizar según su número:\n" +
+                    "1.- Agregar un libro.\n" +
+                    "2.- Eliminar un libro.\n" +
+                    "3.- Realizar un préstamo.\n" +
+                    "4.- Devolver un libro.\n" +
+                    "5.- Consultar el estado de los libros.\n" +
+                    "6.- Salir del gestor de biblioteca."
+        )
+        val opcion = readln().toInt()
+        return opcion
+    }
+
+    // Menú de usuario:
+    var opcion : Int
+    var seguir = true
+    while (seguir) {
+        opcion = mostrarMenu()
+        when (opcion) {
+            // Agregar Libro
+            1 -> // TODO agregar un libro manualmente
+
+            2 -> // TODO eliminar un libro, por autor o titulo
+
+
+
+            3 -> // TODO registrar prestamo por titulo del libro
+
+
+            4  -> // TODO Resgistrar devolución por título del libro
+
+
+            5 -> // TODO mostrar los libros según su estado
+                // Salir del programa
+            6 -> "Saliendo del programa"
+            else -> "Opción inválida. Por favor elija un número disponible en el menu de gestión."
+        }
+    }
 }
